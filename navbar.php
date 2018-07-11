ecc8d7da941f19b11dc6ba5b84d0b23d6c0dc753
DAVID CASADIEGOS <david.2818@outlook.com>
2018-07-11 15:03:19 -0500

    nabvar

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top default" style="position:absolute; width: 100%; position: fixed; background-color: rgba(0.94, 0.33, 0.31, 0.6) !important;">
+    <a class="navbar-brand" href="#">
+        <img src="img/logos/logo.png" class="margintop--12" alt="" width="150" height="60"/>
+    </a>
+    <ul class="navbar-nav ">
+        <li class="nav-item" style="border: 1px solid white; border-radius: 5px; padding-left: 5px; padding-right: 5px; margin: 3px;">
+            <a class="nav-link" href="#cont-colombia" style="color: white;">Colombia</a>
+        </li>
+        <li class="nav-item" style="border: 1px solid white; border-radius: 5px; padding-left: 5px; padding-right: 5px; margin: 3px;">
+            <a class="nav-link" href="#cont-peru" style="color: white;">Perú</a>
+        </li>
+    </ul>
+</nav>
